9eb8660c5c0aea97b6c54ea1cd139e8016f81569
caqueiro <cassiusbessa@gmail.com>
2024-03-14 21:00:17 -0300

    fix image norminnete

--- image.c
+++ image.c
@@ -6,7 +6,7 @@
 /*   By: caqueiro <caqueiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 21:26:44 by caqueiro          #+#    #+#             */
-/*   Updated: 2024/03/14 20:31:38 by caqueiro         ###   ########.fr       */
+/*   Updated: 2024/03/14 21:00:01 by caqueiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ t_img	new_image(t_mlx_map m)
 	t_img	img;
 
 	img.mlx_img = mlx_new_image(m.data.mlx, m.map->window_size.width * 2,
-		m.map->window_size.height * 2);
-	
-	img.addr = mlx_get_data_addr(img.mlx_img, &img.bpp, &img.line_len, &img.endian);
+			m.map->window_size.height * 2);
+	img.addr = mlx_get_data_addr(img.mlx_img, &img.bpp, &img.line_len,
+			&img.endian);
 	return (img);
 }
 
@@ -32,18 +32,19 @@ int	select_color(int z)
 
 void	img_pix_put(t_img *img, int x, int y, int z)
 {
-    char    *pixel;
-    int		i;
+	char	*pixel;
+	int		i;
 	int		color;
-    i = img->bpp - 8;
-    pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+
+	i = img->bpp - 8;
+	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
 	color = select_color(z);
-    while (i >= 0)
-    {
-        if (img->endian != 0 && pixel > img->addr)
-            *pixel++ = (color >> i) & 0xFF;
-        else if (pixel > img->addr)
-            *pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
-        i -= 8;
-    }
+	while (i >= 0)
+	{
+		if (img->endian != 0 && pixel > img->addr)
+			*pixel++ = (color >> i) & 0xFF;
+		else if (pixel > img->addr)
+			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
+		i -= 8;
+	}
 }
