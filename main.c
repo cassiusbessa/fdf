c1e7c10d218f93a595fcc587597d9bf78181907f
caqueiro <cassiusbessa@gmail.com>
2024-03-14 21:16:57 -0300

    remove not used files

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: caqueiro <caqueiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 17:09:41 by caqueiro          #+#    #+#             */
-/*   Updated: 2024/03/12 19:36:37 by caqueiro         ###   ########.fr       */
+/*   Updated: 2024/03/14 21:14:35 by caqueiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,13 @@
 int	main(int argc, char **argv)
 {
 	t_map	*map;
+	int		fd;
 
+	if (argc != 2)
+		return (0);
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		return (0);
+	close(fd);
 	mlx_config(argv[1]);
 }
